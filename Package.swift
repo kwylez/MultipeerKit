066473720e7d2ad53e0dd4e20e80ce5aa301fea9
Cory D. Wiles <corywiles@icloud.com>
2020-06-30 22:52:31 -0400

    remove dynic

--- Package.swift
+++ Package.swift
@@ -14,11 +14,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MultipeerKit",
-            targets: ["MultipeerKit"]),
-        .library(
-            name: "MultipeerKit-dynamic",
-            type: .dynamic,
-            targets: ["MultipeerKit"]),
+            targets: ["MultipeerKit"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
